cf3cb1cc7500527fc76cd2a5e2ef5d7a1321d0bb
kevilu9116 <kevin.n.lu123@gmail.com>
2014-08-29 12:25:12 -0400

    Testing commit

--- TCI.py
+++ TCI.py
@@ -21,6 +21,7 @@ import scipy as s
 from util import *
 
 ###############################################################################################
+#Test Git Commit
 """
  The following block define the Theano functions that going to be used in the regular python funcitons
  """
